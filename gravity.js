5c97f143e86a9d990580749ea1a50fb3a63ae6aa
Joshua Perry <josh@6bit.com>
2025-03-12 22:34:17 -0600

    Use closure to remove globals and make code reentrant

--- gravity.js
+++ gravity.js
@@ -1,40 +1,40 @@
-let bound  // the bounding rect of the canvas
-function gravitate() {
-    canvas.addEventListener('mousedown', click)
-    canvas.addEventListener('mouseup', unclick)
-    canvas.addEventListener('mousemove', drag)
-    bound = canvas.getBoundingClientRect();
-}
-
-let dragging = false
-let start = {x: 0, y: 0}
-function click(event) {
-    dragging = true
-    start = offset(event)
-}
-
 /**
  This offsets a mouse event x,y by the canvas position
 */
-function offset({x, y}){
+function offset({target, x, y}){
+    bound = target.getBoundingClientRect();
     return {
         x: x - bound.left,
         y: y - bound.top
     }
 }
 
-function drag(event) {
-    if(dragging) {
-        const current = offset(event)
-        const ctx = canvas.getContext('2d')
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-        ctx.fillStyle = 'green'
-        ctx.fillRect(start.x, start.y, current.x - start.x, current.y - start.y)
+function gravitator(canvas) {
+    let dragging = false
+    let start = {x: 0, y: 0}
+    function click(event) {
+        dragging = true
+        start = offset(event)
     }
-}
 
-function unclick(event) {
-    dragging = false
+    function drag(event) {
+        if(dragging) {
+            const current = offset(event)
+            const canvas = event.target
+            const ctx = canvas.getContext('2d')
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            ctx.fillStyle = 'green'
+            ctx.fillRect(start.x, start.y, current.x - start.x, current.y - start.y)
+        }
+    }
+
+    function unclick(_event) {
+        dragging = false
+    }
+
+    canvas.addEventListener('mousedown', click)
+    canvas.addEventListener('mouseup', unclick)
+    canvas.addEventListener('mousemove', drag)
 }
 
 function popup(message) {
